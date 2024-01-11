c7a0386510a571d88172376be579f1bd7d934a62
ashsajal1 <ashsajal1@gmail.com>
2024-01-11 13:18:00 +0800

    Change html file name to man.html

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const io = new Server(server);
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "index.html"));
+  res.sendFile(join(__dirname, "main.html"));
 });
 
 io.on("connection", (socket) => {
